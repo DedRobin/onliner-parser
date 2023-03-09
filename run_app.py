cc0a9fa3e08b390386bdcb47ca90f52269b22ae2
Pavel Rabekin <pavelrabekin@gmail.com>
2023-03-09 15:55:23 +0300

    moved logger

--- run_app.py
+++ run_app.py
@@ -5,11 +5,12 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 from db.tools import create_session
 from bots.commands import start
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-    )
     session = create_session()
 
     application = ApplicationBuilder().token(os.environ.get("BOT_TOKEN")).build()
